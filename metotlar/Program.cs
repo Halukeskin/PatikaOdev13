1a3551400137777ef2a547de1c46221281730e76
Keskin6161 <76918333+Keskin6161@users.noreply.github.com>
2021-05-21 14:01:03 +0300

    Add files via upload

---
+++ metotlar/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace metodlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2;
+            int b= 3;
+
+            Console.WriteLine(a+b);
+
+            int sonuc = topla (a,b);
+            Console.WriteLine(sonuc);
+
+            Metotlar ornek = new Metotlar();
+            ornek.EkranaYazdir(Convert.ToString(sonuc));
+        }
+        static int topla(int deger1, int deger2)
+        {
+            return(deger1 + deger2);
+        }
+    }
+    class Metotlar
+    {
+        public void EkranaYazdir(string veri)
+        {
+            Console.WriteLine(veri);
+        }
+
+        public int ArttirVeTopla(int deger1, int deger2)
+        {
+            deger1 +=1;
+            deger2 +=1;
+            return deger1 + deger2;
+        }
+    }
+}
